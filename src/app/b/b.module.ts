9a5c7f451b1827fe6bb0d284ecc317ddaf122d26
Santiago Gomez <josagosagomez@gmail.com>
2024-05-02 21:55:24 -0600

    Modules, components and routes

---
+++ src/app/b/b.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BRoutingModule } from './b-routing.module';
+import { B1Component } from './components/b1/b1.component';
+import { B2Component } from './components/b2/b2.component';
+import { B3Component } from './components/b3/b3.component';
+import { ZModule } from '../z/z.module';
+
+
+@NgModule({
+  declarations: [
+    B1Component,
+    B2Component,
+    B3Component,
+  ],
+  imports: [
+    CommonModule,
+    BRoutingModule,
+    ZModule
+     
+  ]
+})
+export class BModule { }
